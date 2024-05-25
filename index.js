f1e8a02405feeaa793c3a654da200ab01b04fc44
Daniel <guardians01@outlook.com>
2024-05-25 13:57:19 -0600

    Se agregó la búsqueda por id de medicamento.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const laboratorios = require('./routes/laboratorios');
 const medicamentoRoute = require('./routes/busquedaNombreMedicamento');
 const laboratorioRoute = require('./routes/busquedaLaboratorio');
 const sustanciaRoute = require('./routes/busquedaSustanciaActiva');
+const medicamentoId = require('./routes/busquedaMedicamentoPorId');
 
 const app = express();
 const port = 3001;
@@ -17,8 +18,10 @@ app.use('/api/laboratorios', laboratorios);
 // Rutas relacionadas con las búsquedas
 // Nombre de medicamento
 app.use('/api/busqueda/medicamento', medicamentoRoute);
+app.use('/api/busqueda/medicina', medicamentoId);
 app.use('/api/busqueda/laboratorio', laboratorioRoute);
 app.use('/api/busqueda/sustancia', sustanciaRoute);
+
 
 
 app.listen(port, () => {
---
+++ routes/busquedaMedicamentoPorId.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const router = express.Router();
+const db = require('../database/config');
+
+router.get('/', (req, res) => {
+  const datoABuscar = req.query.dato;
+  
+  const query = `SELECT Medicamento.nombre, Medicamento.descripcion, Medicamento.indicaciones, Inventario.cantidad, Inventario.precioVenta, Inventario.caducidad FROM Medicamento INNER JOIN Inventario ON Medicamento.id = Inventario.idMedicamento WHERE Medicamento.id = ${ datoABuscar };`;
+  
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error interno del servidor');
+      return;
+    }
+    
+    res.json(results);
+  });
+});
+
+module.exports = router;
