ce11078d1249e90dc24c7841b75ece9841907d65
Daniel <guardians01@outlook.com>
2024-07-23 18:11:39 -0600

    Ya registra medicamento nuevo.

--- controllers/events/new/medicamento.js
+++ controllers/events/new/medicamento.js
@@ -1,8 +1,8 @@
-// controllers/events/new/medicamentos.js
 const multer = require('multer');
 const Medicamento = require('../../../models/medicamento');
 const Sustancia = require('../../../models/sustancias');
 const Laboratorio = require('../../../models/laboratorios');
+const Presentacion = require('../../../models/presentaciones');
 
 // Configuración de multer para manejar la carga de archivos
 const storage = multer.memoryStorage();
@@ -15,8 +15,18 @@ const createMedicament = async (req, res) => {
         }
 
         try {
-            const { nombre, nombreSustancia, nombrePresentacion, nombreLaboratorio, descripcion, indicaciones } = req.body;
+            const { nombre, sustancia: nombreSustancia, presentacion: nombrePresentacion, laboratorio: nombreLaboratorio, descripcion, indicaciones } = req.body;
             const imagen = req.file ? req.file.buffer : null;
+
+            console.log("Valores recibidos:", {
+                nombre,
+                nombreSustancia,
+                nombrePresentacion,
+                nombreLaboratorio,
+                descripcion,
+                indicaciones,
+                imagen
+            });
 
             // Buscar el ID de la sustancia por nombre
             const sustancia = await Sustancia.findOne({ where: { nombre: nombreSustancia } });
@@ -24,7 +34,7 @@ const createMedicament = async (req, res) => {
                 return res.status(404).json({ ok: false, error: 'Sustancia no encontrada.' });
             }
             const idSustancia = sustancia.id;
-            console.log(idSustancia);
+            console.log("ID Sustancia:", idSustancia);
 
             // Buscar el ID de la presentación por nombre
             const presentacion = await Presentacion.findOne({ where: { nombre: nombrePresentacion } });
@@ -32,7 +42,7 @@ const createMedicament = async (req, res) => {
                 return res.status(404).json({ ok: false, error: 'Presentación no encontrada.' });
             }
             const idPresentacion = presentacion.id;
-            console.log(idPresentacion);
+            console.log("ID Presentacion:", idPresentacion);
 
             // Buscar el ID del laboratorio por nombre
             const laboratorio = await Laboratorio.findOne({ where: { nombre: nombreLaboratorio } });
@@ -40,7 +50,7 @@ const createMedicament = async (req, res) => {
                 return res.status(404).json({ ok: false, error: 'Laboratorio no encontrado.' });
             }
             const idLaboratorio = laboratorio.id;
-            console.log(idLaboratorio);
+            console.log("ID Laboratorio:", idLaboratorio);
             
             // Crear el nuevo medicamento
             const nuevoMedicamento = await Medicamento.create({
